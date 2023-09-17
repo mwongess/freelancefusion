9bf1be9f86b04771a8fb23a79082073b12678f3e
mwongess <amosmwongelah@gmail.com>
2023-09-17 19:56:23 +0300

    update color

--- client/src/app/(pages)/login/page.tsx
+++ client/src/app/(pages)/login/page.tsx
@@ -7,17 +7,17 @@ const Login = () => {
     <div className='flex h-screen'>
       <Notes />
       <div className='flex flex-col justify-center items-center w-1/2 bg-white '>
-        <form action="" className='w-1/2 text-black'>
+        <form action="" className='w-[60%] text-black'>
           <div className='flex flex-col gap-2'>
-            <label htmlFor="">Email</label>
-            <input className='p-2 bg-transparent border border-[#0B9240] ' type="email" />
+            <label className='font-bold text-lg' htmlFor="">Email</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240] ' type="email" />
           </div>
           <div className='flex flex-col gap-2 my-3'>
-            <label htmlFor="">Password</label>
-            <input className='p-2 bg-transparent border border-[#0B9240]' type="password" />
+            <label className='font-bold text-lg' htmlFor="">Password</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240]' type="password" />
           </div>
           <div className='mb-3'>
-            <button className='bg-[#0B9240] rounded-lg py-2 px-6'>Login</button>
+            <button className='text-white bg-[#0B9240] rounded-lg py-2 px-6'>Login</button>
           </div>
           <div>
             <p>Dont have an account? register <Link className="underline" href="/signup">here.</Link></p>
--- client/src/app/(pages)/signup/page.tsx
+++ client/src/app/(pages)/signup/page.tsx
@@ -6,25 +6,25 @@ const Signup = () => {
     <div className='flex h-screen'>
       <Notes />
       <div className='flex flex-col justify-center items-center w-1/2 bg-white '>
-        <form action="" className='w-1/2 text-black'>
+        <form action="" className='w-[60%] text-black'>
           <div className='flex flex-col gap-2'>
-            <label htmlFor="">Name</label>
-            <input className='p-2 bg-transparent border border-[#0B9240] ' type="text" />
+            <label className='font-bold text-lg' htmlFor="">Name</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240] ' type="text" />
           </div>
           <div className='flex flex-col gap-2'>
-            <label htmlFor="">Email</label>
-            <input className='p-2 bg-transparent border border-[#0B9240]' type="email" />
+            <label className='font-bold text-lg' htmlFor="">Email</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240]' type="email" />
           </div>
           <div className='flex flex-col gap-2 my-3'>
-            <label htmlFor="">Password</label>
-            <input className='p-2 bg-transparent border border-[#0B9240]' type="password" />
+            <label className='font-bold text-lg' htmlFor="">Password</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240]' type="password" />
           </div>
           <div className='flex flex-col gap-2 my-3'>
-            <label htmlFor="">Confirm Password</label>
-            <input className='p-2 bg-transparent border border-[#0B9240]' type="password" />
+            <label className='font-bold text-lg' htmlFor="">Confirm Password</label>
+            <input className='p-2 bg-transparent border-2 border-[#0B9240]' type="password" />
           </div>
           <div className='mb-3'>
-            <button className='bg-[#0B9240] rounded-lg py-2 px-6'>Register</button>
+            <button className='text-white bg-[#0B9240] rounded-lg py-2 px-6'>Register</button>
           </div>
           <div>
             <p>Already have an account? Login <Link className="underline" href="/login">here.</Link></p>
