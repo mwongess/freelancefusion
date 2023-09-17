7c190d5d46746adbd99e95387694220764e504ca
mwongess <amosmwongelah@gmail.com>
2023-09-17 17:30:22 +0300

    add login inputs

--- client/src/app/(pages)/login/page.tsx
+++ client/src/app/(pages)/login/page.tsx
@@ -5,7 +5,22 @@ const Login = () => {
   return (
     <div className='flex h-screen'>
       <Notes />
-      <div className='w-1/2 bg-white '></div>
+      <div className='flex flex-col justify-center items-center w-1/2 bg-white '>
+        <form action="" className='w-1/2'>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="">Email</label>
+            <input className='p-2 bg-transparent border border-[#0B9240] rounded ' type="email" />
+          </div>
+          <div className='flex flex-col gap-2 my-3'>
+            <label htmlFor="">Password</label>
+            <input className='p-2 bg-transparent border border-[#0B9240] rounded' type="password" />
+          </div>
+          <div className=''>
+            <button className='bg-[#0B9240] rounded-lg py-2 px-6'>Login</button>
+          </div>
+        </form>
+
+      </div>
     </div>
   )
 }
