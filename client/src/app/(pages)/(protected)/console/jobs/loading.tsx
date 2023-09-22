edc3a40a30312c4e5742922ebe39e95833964f52
SamuelOkoth <okothsamuel78@gmail.com>
2023-09-22 05:08:20 -0600

    update index

--- client/src/app/(pages)/(protected)/console/jobs/loading.tsx
+++ client/src/app/(pages)/(protected)/console/jobs/loading.tsx
@@ -5,7 +5,7 @@ const JobsLoader = () => {
     <div className='p-8'>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
       {Array.from({ length: 4 }).map((_, index) => (
-        <div className='bg-gray-400 animate-pulse h-[300px] p-4 rounded-lg'>
+        <div key={index} className='bg-gray-400 animate-pulse h-[300px] p-4 rounded-lg'>
 
         </div>
       ))}
