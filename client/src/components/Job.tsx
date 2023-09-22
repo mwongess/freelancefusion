2ea3fc97b5caad82a4901003255c5363cd1d9ef1
SamuelOkoth <okothsamuel78@gmail.com>
2023-09-22 09:54:18 -0600

    update jobs

--- client/src/components/Job.tsx
+++ client/src/components/Job.tsx
@@ -1,16 +1,15 @@
-const Job = () => {
+const Job = ({job}: any) => {
     return (
         <div className="flex flex-col gap-4 p-4 border rounded-lg">
-            <h1>Lorem ipsum dolor sit.</h1>
+            <h1>{job.title}</h1>
             <div className="flex items-center gap-3">
                 <p className="bg-[blue] rounded-full py-1 px-3 ">New</p>
-                <p>Fixed price -</p>
-                <p>Posted 1 sec ago</p>
+                <p>Fixed price</p>
             </div>
             <div >
                 <div className="flex gap-10 mb-3">
                     <div>
-                        <p>$20</p>
+                        <p>$ {job.price}</p>
                         <p>Fixed price</p>
                     </div>
                     <div>
@@ -18,15 +17,15 @@ const Job = () => {
                         <p>Experince level</p>
                     </div>
                 </div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut dolorum, aliquid repellendus amet culpa consequuntur ducimus aspernatur a modi quod ea sed voluptatibus doloremque veritatis quas libero vel distinctio ipsa accusamus incidunt. Quidem facilis dignissimos e dolorem!</p>
-                <div className="flex flex-wrap gap-3 my-3">
+                <p>{job.description}</p>
+                {/* <div className="flex flex-wrap gap-3 my-3">
                     <p className="text-black bg-slate-100 rounded-full py-1 px-7 w-fit">ts</p>
                     <p className="text-black bg-slate-100 rounded-full py-1 px-7 w-fit">js</p>
                     <p className="text-black bg-slate-100 rounded-full py-1 px-7 w-fit">Next</p>
                     <p className="text-black bg-slate-100 rounded-full py-1 px-7 w-fit">Appwrite</p>
 
-                </div>
-                <button className="rounded-full  py-1 px-6 text-white bg-[#0B9240]">See More</button>
+                </div> */}
+                {/* <button className="rounded-full  py-1 px-6 text-white bg-[#0B9240]">See More</button> */}
             </div>
         </div>
     )
--- client/src/components/JobList.tsx
+++ client/src/components/JobList.tsx
@@ -1,11 +1,39 @@
 import Job from "./Job"
 
+
 const JobList = () => {
-  const jobs = [{}]
+  const jobs =[
+    {
+      "title": "Website Design",
+      "description": "Design a responsive and visually appealing website for my business. Must include logo design and integration with content management system (CMS).",
+      "price": "$500 - $1,500"
+    },
+    {
+      "title": "Content Writing",
+      "description": "Write 10 high-quality blog posts (500-800 words each) on topics related to technology and digital marketing.",
+      "price": "$200 - $400"
+    },
+    {
+      "title": "Logo Design",
+      "description": "Create a unique and memorable logo for my startup. Provide multiple design concepts for review and revisions.",
+      "price": "$100 - $300"
+    },
+    {
+      "title": "Mobile App Development",
+      "description": "Develop a cross-platform mobile app for iOS and Android that allows users to track fitness activities and set goals.",
+      "price": "$2,000 - $5,000"
+    },
+    {
+      "title": "Social Media Management",
+      "description": "Manage social media accounts (Facebook, Twitter, Instagram) for a small business. Create and schedule posts, engage with followers, and track analytics.",
+      "price": "$300 - $800 per month"
+    },
+  
+  ]
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
-      {Array.from({ length: 4 }).map((_, index) => (
-        < Job key={index} />
+      {jobs.map((job,index) => (
+        < Job  key={index} job={job}/>
       ))}
     </div>
   )
