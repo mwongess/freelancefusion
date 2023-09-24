e32552bea5fc2aab773c0753c16f9440000e9752
Samuel Okoth Ochiel <50865774+SamuelOkoth@users.noreply.github.com>
2023-09-24 14:15:06 +0300

    Update Services.tsx

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -1,7 +1,7 @@
 const Services = () => {
     return (
         <div id="services" className="text-center">
-            <h1 className='font-bold text-2xl'>Sercices</h1>
+            <h1 className='font-bold text-2xl'>Services</h1>
             <div className=" grid grid-cols-1 sm:grid-cols-4 gap-3 px-8 py-20  text-center ">
                 <div className="border rounded p-3 h-fit">
                     <h1>Graphics Design</h1>
